2c11155dba6f857e7a879ca8adb5e99620540f06
Emily Peregrine <tomfperegrine@googlemail.com>
2015-09-04 21:11:38 +0100

    Added Starsign ranges class

--- Starsign/Starsigns.cs
+++ Starsign/Starsigns.cs
@@ -34,6 +34,23 @@ namespace Starsign
 
 	public static class Starsigns
 	{
+		private const int YEAR = 2015;
+
+		#region StarsignRanges
+		public static DateRange AquariusRange = new DateRange(new DateTime(YEAR, 1, 21), new DateTime(YEAR, 2, 19));
+		public static DateRange PiscesRange = new DateRange(new DateTime(YEAR, 2, 20), new DateTime(YEAR, 3, 20));
+		public static DateRange AriesRange = new DateRange(new DateTime(YEAR, 3, 21), new DateTime(YEAR, 4, 20));
+		public static DateRange TaurusTange = new DateRange(new DateTime(YEAR, 4, 21), new DateTime(YEAR, 5, 21));
+		public static DateRange GeminiRange = new DateRange(new DateTime(YEAR, 5, 22), new DateTime(YEAR, 6, 21));
+		public static DateRange CancerRange = new DateRange(new DateTime(YEAR, 6, 22), new DateTime(YEAR, 7, 23));
+		public static DateRange LeoRange = new DateRange(new DateTime(YEAR, 7, 24), new DateTime(YEAR, 8, 23));
+		public static DateRange VirgoRange = new DateRange(new DateTime(YEAR, 8, 24), new DateTime(YEAR, 9, 23));
+		public static DateRange LibraRange = new DateRange(new DateTime(YEAR, 9, 24), new DateTime(YEAR, 10, 23));
+		public static DateRange ScorpioRange = new DateRange(new DateTime(YEAR, 10, 24), new DateTime(YEAR, 11, 22));
+		public static DateRange SagittariusRange = new DateRange(new DateTime(YEAR, 11, 23), new DateTime(YEAR, 12, 21));
+		public static DateRange CapricornRange = new DateRange(new DateTime(YEAR, 12, 22), new DateTime(YEAR, 1, 20));
+		#endregion
+
 		public static Starsign GetSign(DateTime d) 
 		{
 			throw new NotImplementedException ();
