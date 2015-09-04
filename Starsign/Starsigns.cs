afc556cb757250a359809d7427ea7b4b1a958204
Emily Peregrine <tomfperegrine@googlemail.com>
2015-09-04 19:32:23 +0100

    Added starsign enum

---
+++ Starsign/Starsigns.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Starsign
+{
+	public enum Starsign {
+		Aquarius,
+		Pisces,
+		Aries,
+		Taurus,
+		Gemini,
+		Cancer,
+		Leo,
+		Virgo,
+		Libra,
+		Scorpio,
+		Sagittarius,
+		Capricorn,
+		Error
+	}
+
+	public static class Starsigns
+	{
+		public static Starsign GetSign(DateTime d) 
+		{
+			throw new NotImplementedException ();
+		}
+	}
+}
+
