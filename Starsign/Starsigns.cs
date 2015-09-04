a4b9f411dcb6dc74e3b176e43c4bfba2ca2594b6
Emily Peregrine <tomfperegrine@googlemail.com>
2015-09-04 20:11:22 +0100

    Added DateRange class

--- Starsign/Starsigns.cs
+++ Starsign/Starsigns.cs
@@ -18,6 +18,20 @@ namespace Starsign
 		Error
 	}
 
+	public class DateRange {
+		public DateTime Min;
+		public DateTime Max;
+
+		public DateRange (DateTime _Min, DateTime _Max) {
+			Min = _Min;
+			Max = _Max;
+		}
+
+		public bool isInRange (DateTime day) {
+			return (Min <= day) && (day <= Max);
+		}
+	}
+
 	public static class Starsigns
 	{
 		public static Starsign GetSign(DateTime d) 
