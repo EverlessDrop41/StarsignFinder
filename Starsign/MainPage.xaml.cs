8064b60eb1d6f56da74d48fbc39fca96b29c5097
Emily Peregrine <tomfperegrine@googlemail.com>
2015-09-04 18:56:17 +0100

    Basic setup complete

---
+++ Starsign/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+using Xamarin.Forms;
+
+namespace Starsign
+{
+	public partial class MainPage : ContentPage
+	{
+		DatePicker BirthDate;
+
+		public MainPage ()
+		{
+			InitializeComponent ();
+			BirthDate = BirthDatePicker;
+		}
+
+		public void OnBirthDateChanged(object sender, DateChangedEventArgs dcea) 
+		{
+			if (dcea.NewDate.ToString("M") != dcea.OldDate.ToString("M")) {
+				DisplayAlert ("Date Changed", dcea.NewDate.ToString("M") , "OK");
+				BirthDate.Date = dcea.NewDate;
+			}
+		}
+	}
+}
+
